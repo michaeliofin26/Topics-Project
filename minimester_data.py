5dce7d5b246cf6076589352f284a5c0ef150de53
michaeliofin26 <michael.iofin26@trinityschoolnyc.org>
2026-01-05 10:35:32 -0500

    randomly generated student choices

--- minimester_data.py
+++ minimester_data.py
@@ -1,1 +1,30 @@
+import random
+import numpy as np
+from scipy.optimize import linprog
+
+
+#list of length 35, has each minimester probability
+
+num_students = 400
+num_minis = 35
+
+minimesters=list(range(num_minis))
+minimester_popularities=[]
+sum = 0
+
+for i in range(num_minis):
+    n=random.random()
+    minimester_popularities.append(n)
+    sum+=n
+
+for i in range(num_minis):
+    minimester_popularities[i]/=sum
+
+students = list(range(num_students))
+
+student_choices = []
+
+for i in students:
+    new_choice = np.random.choice(minimesters,size=10,replace=False,p=minimester_popularities).tolist()
+    student_choices.append(new_choice)
 
